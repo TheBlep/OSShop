50f96d569201b77b3668fedeff0aedf281026081
TheBlep <soshire@protonmail.com>
2025-04-10 11:19:32 +0200

    edit: add cloudinary functionality

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import handler404
+import cloudinary
+import cloudinary.uploader
+from cloudinary.utils import cloudinary_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
